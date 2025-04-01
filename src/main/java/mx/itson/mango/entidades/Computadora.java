998ed656ae89ccf6e16caeeb4de84f5bf4db5e5c
alumnog <alumnog@S921-7.itson.edu.mx>
2025-04-01 10:29:11 -0700

    Mango

---
+++ src/main/java/mx/itson/mango/entidades/Computadora.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.mango.entidades;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumnog
+ */
+public class Computadora extends DispositivoElectronico {
+    
+    private int memoriaRAM;
+    private int espacioDisco;
+    
+    public Computadora(String marca, String modelo, String color, int memoriaRAM, int espacioDisco){
+    super(marca, modelo, color);
+    this.memoriaRAM = memoriaRAM;
+    this.espacioDisco = espacioDisco;
+    }
+
+    
+   // public static double espacioDisco(double espacioLibre){
+     //  return espacioLibre = espacioDisco - pesoAplicacion
+    
+}
+
+
